2da621d78dbbecaf69489a2c2a491007f75398de
swami <swaminaidu.seepana@nstarxinc.com>
2023-04-18 15:55:07 +0530

    commit by X

--- src/test/java/utils/GenericUtils.java
+++ src/test/java/utils/GenericUtils.java
@@ -1,5 +1,7 @@
 package utils;
 
+import java.util.Set;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -11,6 +13,16 @@ public class GenericUtils {
 	public GenericUtils(WebDriver driver, WebDriverWait wait) {
 		this.driver = driver;
 		this.wait = wait;
+	}
+	
+	public void switchWindows() {
+		String w1 = driver.getWindowHandle(), w2="";
+		Set<String> w = driver.getWindowHandles();
+		for(String i: w) {
+			if(!i.equals(w1))
+				w2 = i;
+		}
+		driver.switchTo().window(w2);
 	}
 
 }
